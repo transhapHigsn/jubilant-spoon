d6ce7cb65453de9e5b994931a9ed61712134107c
transhap <fet.prashantsingh@gmail.com>
2019-03-10 01:34:41 +0530

    Learned more about macros, quote and unquote

--- getting_started/lib/macro1.ex
+++ getting_started/lib/macro1.ex
@@ -5,3 +5,12 @@ defmodule GettingStarted.Hello do
     end
   end
 end
+
+
+defmodule GettingStarted.MyMacro do
+  defmacro unless(expr, do: block) do
+    quote do
+      if !unquote(expr), do: unquote(block)
+    end
+  end
+end
